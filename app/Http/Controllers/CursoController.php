44f6f0e3f5c629f483f7570d57194180d1a220ff
edverde <eduardvr83@gmail.com>
2022-06-02 12:52:10 +0200

    afegint rutes

---
+++ app/Http/Controllers/CursoController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CursoController extends Controller
+{
+    public function index(){
+        return "Bienvenido a la página cursos";
+    }
+    
+
+    public function create(){
+        return 'en esta pagina podras crear un curso';
+    }
+
+    public function show($curso){
+        return "Bienvenido al curso: $curso";
+    }
+}
